7a0520468a6e63e208d5542ba0b76fde3745897b
prmncr <stremousov02@mail.ru>
2022-04-08 00:22:48 +0500

    finally, hex file

--- src/main/java/com/prmncr/normativecontrol/dbos/ProcessedDocument.java
+++ src/main/java/com/prmncr/normativecontrol/dbos/ProcessedDocument.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ByteArraySerializer;
 import com.prmncr.normativecontrol.dtos.Error;
+import com.prmncr.normativecontrol.serializers.ByteArraySerializer;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
