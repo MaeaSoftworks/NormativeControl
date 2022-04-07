8f983d2e190f9a4223278fa9b079c61ad8b03140
prmncr <stremousov02@mail.ru>
2022-04-07 22:36:32 +0500

    hex file

--- src/main/java/com/prmncr/normativecontrol/serializers/ByteArraySerializer.java
+++ src/main/java/com/prmncr/normativecontrol/serializers/ByteArraySerializer.java
@@ -7,13 +7,16 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import java.io.IOException;
 
 public class ByteArraySerializer extends JsonSerializer<byte[]> {
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
     @Override
     public void serialize(byte[] bytes, JsonGenerator gen, SerializerProvider provider) throws IOException {
-        gen.writeStartArray();
-        for (byte b : bytes) {
-            gen.writeNumber(b & 0xFF);
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
         }
-        gen.writeEndArray();
+        gen.writeString(new String(hexChars));
     }
 }
