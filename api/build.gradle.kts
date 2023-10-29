bed399bc678aae611109232ff54acfee972db46c
prmncr <39909848+prmncr@users.noreply.github.com>
2023-10-29 20:55:10 +0500

    buildscripts cleanup

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,16 +1,61 @@
 plugins {
     application
-    id("org.springframework.boot") version "3.1.1"
-    id("io.spring.dependency-management") version "1.1.0"
+
     kotlin("jvm") version "1.9.20-RC2"
     kotlin("plugin.spring") version "1.9.10"
     kotlin("plugin.serialization") version "1.9.10"
+
+    id("org.springframework.boot") version "3.1.1"
+    id("io.spring.dependency-management") version "1.1.0"
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_21
 
-application {
-    mainClass.set("api.NormativeControlApiApplicationKt")
+application.mainClass.set("api.NormativeControlApiApplicationKt")
+
+
+@Suppress("REDUNDANT_LABEL_WARNING")
+dependencies {
+    implementation@run {
+        implementation(project(":core"))
+
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+        implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+        implementation("org.springframework.boot:spring-boot-starter-webflux")
+        implementation("org.springframework.boot:spring-boot-starter-security")
+        implementation("org.springframework.boot:spring-boot-starter-validation")
+        implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+
+        implementation("io.r2dbc:r2dbc-pool")
+        implementation("io.asyncer:r2dbc-mysql")
+        implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+        implementation("software.amazon.awssdk:s3:2.20.121")
+        implementation("software.amazon.awssdk:netty-nio-client:2.20.121")
+    }
+
+    runtime@run {
+        runtimeOnly("com.mysql:mysql-connector-j")
+        runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+        runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    }
+
+    dev@run {
+        developmentOnly("org.springframework.boot:spring-boot-devtools")
+    }
+
+    annotations@run {
+        annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    }
+
+    test@run {
+        testImplementation("io.projectreactor:reactor-test")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.springframework.security:spring-security-test")
+    }
 }
 
 configurations {
@@ -19,38 +64,6 @@ configurations {
     }
 }
 
-dependencies {
-    implementation(project(":core"))
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("software.amazon.awssdk:s3:2.20.121")
-    implementation("software.amazon.awssdk:netty-nio-client:2.20.121")
-
-    implementation("io.r2dbc:r2dbc-pool")
-    implementation("io.asyncer:r2dbc-mysql")
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-
-    runtimeOnly("com.mysql:mysql-connector-j")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
-
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-    testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
-}
-
-tasks.withType<Test> {
+tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,21 +1,18 @@
 plugins {
-    application
     kotlin("jvm") version "1.9.20-RC2"
     kotlin("plugin.serialization") version "1.9.10"
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_21
 
-application {
-    mainClass.set("com.maeasoftworks.normativecontrolcore.bootstrap.NormativeControlApplicationKt")
-}
-
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
     implementation("org.docx4j:docx4j-JAXB-ReferenceImpl:11.4.9")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.getByName<Test>("test") {
