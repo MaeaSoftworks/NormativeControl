ac77f7918063c32e1858cf8d787079e169c4c421
prmncr <39909848+prmncr@users.noreply.github.com>
2024-05-08 19:37:09 +0500

    new message schema

--- launcher/src/main/kotlin/normativecontrol/launcher/client/messages/Job.kt
+++ launcher/src/main/kotlin/normativecontrol/launcher/client/messages/Job.kt
@@ -1,27 +1,18 @@
 package normativecontrol.launcher.client.messages
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
-@Suppress("DEPRECATION")
 @Serializable
 data class Job(
     val userId: Long?,
-    @SerialName("id")
     val documentId: Long,
-    @Deprecated("Use new json schema")
-    @SerialName("source")
-    val sourceDeprecated: String?,
-    @Deprecated("Use new json schema")
-    @SerialName("results")
-    val resultsDeprecated: normativecontrol.launcher.client.messages.Results?
 ) {
     @Transient
     val id = "$userId/$documentId"
 
     @Transient
-    val source = sourceDeprecated ?: "$id.source.docx"
+    val source = "$id.source.docx"
 
     @Transient
     val results = Results(this)
@@ -29,9 +20,9 @@ data class Job(
     @JvmInline
     value class Results(private val job: Job) {
         val docx: String
-            get() = job.resultsDeprecated?.docxDeprecated ?: "${job.id}.result.docx"
+            get() = "${job.id}.result.docx"
 
         val html: String
-            get() = job.resultsDeprecated?.docxDeprecated ?: "${job.id}.result.html"
+            get() = "${job.id}.result.html"
     }
 }
